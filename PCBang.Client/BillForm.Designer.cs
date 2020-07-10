fda25861f6f2775f284d0ad45a2f02cf8c2ac090
p-ekul <luke1682@gmail.com>
2020-07-10 15:33:47 +0900

    end??

--- PCBang.Client/BillForm.Designer.cs
+++ PCBang.Client/BillForm.Designer.cs
@@ -52,6 +52,12 @@
             this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
             this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
             this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControl5 = new DevExpress.XtraLayout.LayoutControl();
+            this.layoutControlItem8 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlGroup4 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.cbbCode = new System.Windows.Forms.ComboBox();
+            this.결제방식 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.bdsCode = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
             this.layoutControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl3)).BeginInit();
@@ -74,15 +80,23 @@
             ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl5)).BeginInit();
+            this.layoutControl5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.결제방식)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsCode)).BeginInit();
             this.SuspendLayout();
             // 
             // layoutControl1
             // 
+            this.layoutControl1.Controls.Add(this.layoutControl5);
             this.layoutControl1.Controls.Add(this.layoutControl3);
             this.layoutControl1.Controls.Add(this.layoutControl2);
             this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.layoutControl1.Location = new System.Drawing.Point(0, 0);
             this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(671, 208, 812, 500);
             this.layoutControl1.Root = this.Root;
             this.layoutControl1.Size = new System.Drawing.Size(509, 774);
             this.layoutControl1.TabIndex = 0;
@@ -94,7 +108,7 @@
             this.layoutControl3.Location = new System.Drawing.Point(12, 12);
             this.layoutControl3.Name = "layoutControl3";
             this.layoutControl3.Root = this.layoutControlGroup2;
-            this.layoutControl3.Size = new System.Drawing.Size(485, 583);
+            this.layoutControl3.Size = new System.Drawing.Size(485, 540);
             this.layoutControl3.TabIndex = 5;
             this.layoutControl3.Text = "layoutControl3";
             // 
@@ -104,12 +118,12 @@
             this.GridBill.Location = new System.Drawing.Point(12, 12);
             this.GridBill.MainView = this.gridView1;
             this.GridBill.Name = "GridBill";
-            this.GridBill.Size = new System.Drawing.Size(461, 559);
+            this.GridBill.Size = new System.Drawing.Size(461, 516);
             this.GridBill.TabIndex = 4;
             this.GridBill.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
             // 
-            // bdsOrder
+            // bdsOrderinBill
             // 
             this.bdsOrderinBill.DataSource = typeof(PC_Project.Data.Order);
             // 
@@ -149,7 +163,7 @@
             this.layoutControlGroup2.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
             this.layoutControlItem3});
             this.layoutControlGroup2.Name = "layoutControlGroup2";
-            this.layoutControlGroup2.Size = new System.Drawing.Size(485, 583);
+            this.layoutControlGroup2.Size = new System.Drawing.Size(485, 540);
             this.layoutControlGroup2.TextVisible = false;
             // 
             // layoutControlItem3
@@ -157,7 +171,7 @@
             this.layoutControlItem3.Control = this.GridBill;
             this.layoutControlItem3.Location = new System.Drawing.Point(0, 0);
             this.layoutControlItem3.Name = "layoutControlItem3";
-            this.layoutControlItem3.Size = new System.Drawing.Size(465, 563);
+            this.layoutControlItem3.Size = new System.Drawing.Size(465, 520);
             this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem3.TextVisible = false;
             // 
@@ -166,16 +180,16 @@
             this.layoutControl2.Controls.Add(this.btnBillOrder);
             this.layoutControl2.Controls.Add(this.btnBillCancel);
             this.layoutControl2.Controls.Add(this.layoutControl4);
-            this.layoutControl2.Location = new System.Drawing.Point(12, 599);
+            this.layoutControl2.Location = new System.Drawing.Point(12, 612);
             this.layoutControl2.Name = "layoutControl2";
             this.layoutControl2.Root = this.layoutControlGroup1;
-            this.layoutControl2.Size = new System.Drawing.Size(485, 163);
+            this.layoutControl2.Size = new System.Drawing.Size(485, 150);
             this.layoutControl2.TabIndex = 4;
             this.layoutControl2.Text = "layoutControl2";
             // 
             // btnBillOrder
             // 
-            this.btnBillOrder.Location = new System.Drawing.Point(12, 124);
+            this.btnBillOrder.Location = new System.Drawing.Point(12, 111);
             this.btnBillOrder.Name = "btnBillOrder";
             this.btnBillOrder.Size = new System.Drawing.Size(360, 27);
             this.btnBillOrder.StyleController = this.layoutControl2;
@@ -185,7 +199,7 @@
             // 
             // btnBillCancel
             // 
-            this.btnBillCancel.Location = new System.Drawing.Point(376, 124);
+            this.btnBillCancel.Location = new System.Drawing.Point(376, 111);
             this.btnBillCancel.Name = "btnBillCancel";
             this.btnBillCancel.Size = new System.Drawing.Size(97, 27);
             this.btnBillCancel.StyleController = this.layoutControl2;
@@ -200,7 +214,7 @@
             this.layoutControl4.Name = "layoutControl4";
             this.layoutControl4.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(917, 333, 812, 500);
             this.layoutControl4.Root = this.layoutControlGroup3;
-            this.layoutControl4.Size = new System.Drawing.Size(461, 108);
+            this.layoutControl4.Size = new System.Drawing.Size(461, 95);
             this.layoutControl4.TabIndex = 4;
             this.layoutControl4.Text = "layoutControl4";
             // 
@@ -209,7 +223,7 @@
             this.lbTotal.BackColor = System.Drawing.Color.White;
             this.lbTotal.Location = new System.Drawing.Point(12, 12);
             this.lbTotal.Name = "lbTotal";
-            this.lbTotal.Size = new System.Drawing.Size(437, 84);
+            this.lbTotal.Size = new System.Drawing.Size(437, 71);
             this.lbTotal.TabIndex = 4;
             this.lbTotal.Text = "합계 : ";
             this.lbTotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -221,7 +235,7 @@
             this.layoutControlGroup3.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
             this.layoutControlItem5});
             this.layoutControlGroup3.Name = "Root";
-            this.layoutControlGroup3.Size = new System.Drawing.Size(461, 108);
+            this.layoutControlGroup3.Size = new System.Drawing.Size(461, 95);
             this.layoutControlGroup3.TextVisible = false;
             // 
             // layoutControlItem5
@@ -229,7 +243,7 @@
             this.layoutControlItem5.Control = this.lbTotal;
             this.layoutControlItem5.Location = new System.Drawing.Point(0, 0);
             this.layoutControlItem5.Name = "layoutControlItem5";
-            this.layoutControlItem5.Size = new System.Drawing.Size(441, 88);
+            this.layoutControlItem5.Size = new System.Drawing.Size(441, 75);
             this.layoutControlItem5.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem5.TextVisible = false;
             // 
@@ -242,7 +256,7 @@
             this.layoutControlItem6,
             this.layoutControlItem7});
             this.layoutControlGroup1.Name = "layoutControlGroup1";
-            this.layoutControlGroup1.Size = new System.Drawing.Size(485, 163);
+            this.layoutControlGroup1.Size = new System.Drawing.Size(485, 150);
             this.layoutControlGroup1.TextVisible = false;
             // 
             // layoutControlItem4
@@ -250,14 +264,14 @@
             this.layoutControlItem4.Control = this.layoutControl4;
             this.layoutControlItem4.Location = new System.Drawing.Point(0, 0);
             this.layoutControlItem4.Name = "layoutControlItem4";
-            this.layoutControlItem4.Size = new System.Drawing.Size(465, 112);
+            this.layoutControlItem4.Size = new System.Drawing.Size(465, 99);
             this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem4.TextVisible = false;
             // 
             // layoutControlItem6
             // 
             this.layoutControlItem6.Control = this.btnBillCancel;
-            this.layoutControlItem6.Location = new System.Drawing.Point(364, 112);
+            this.layoutControlItem6.Location = new System.Drawing.Point(364, 99);
             this.layoutControlItem6.Name = "layoutControlItem6";
             this.layoutControlItem6.Size = new System.Drawing.Size(101, 31);
             this.layoutControlItem6.TextSize = new System.Drawing.Size(0, 0);
@@ -266,7 +280,7 @@
             // layoutControlItem7
             // 
             this.layoutControlItem7.Control = this.btnBillOrder;
-            this.layoutControlItem7.Location = new System.Drawing.Point(0, 112);
+            this.layoutControlItem7.Location = new System.Drawing.Point(0, 99);
             this.layoutControlItem7.Name = "layoutControlItem7";
             this.layoutControlItem7.Size = new System.Drawing.Size(364, 31);
             this.layoutControlItem7.TextSize = new System.Drawing.Size(0, 0);
@@ -278,7 +292,8 @@
             this.Root.GroupBordersVisible = false;
             this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
             this.layoutControlItem1,
-            this.layoutControlItem2});
+            this.layoutControlItem2,
+            this.layoutControlItem8});
             this.Root.Name = "Root";
             this.Root.Size = new System.Drawing.Size(509, 774);
             this.Root.TextVisible = false;
@@ -286,9 +301,9 @@
             // layoutControlItem1
             // 
             this.layoutControlItem1.Control = this.layoutControl2;
-            this.layoutControlItem1.Location = new System.Drawing.Point(0, 587);
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 600);
             this.layoutControlItem1.Name = "layoutControlItem1";
-            this.layoutControlItem1.Size = new System.Drawing.Size(489, 167);
+            this.layoutControlItem1.Size = new System.Drawing.Size(489, 154);
             this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem1.TextVisible = false;
             // 
@@ -297,9 +312,62 @@
             this.layoutControlItem2.Control = this.layoutControl3;
             this.layoutControlItem2.Location = new System.Drawing.Point(0, 0);
             this.layoutControlItem2.Name = "layoutControlItem2";
-            this.layoutControlItem2.Size = new System.Drawing.Size(489, 587);
+            this.layoutControlItem2.Size = new System.Drawing.Size(489, 544);
             this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem2.TextVisible = false;
+            // 
+            // layoutControl5
+            // 
+            this.layoutControl5.Controls.Add(this.cbbCode);
+            this.layoutControl5.Location = new System.Drawing.Point(12, 556);
+            this.layoutControl5.Name = "layoutControl5";
+            this.layoutControl5.Root = this.layoutControlGroup4;
+            this.layoutControl5.Size = new System.Drawing.Size(485, 52);
+            this.layoutControl5.TabIndex = 6;
+            this.layoutControl5.Text = "layoutControl5";
+            // 
+            // layoutControlItem8
+            // 
+            this.layoutControlItem8.Control = this.layoutControl5;
+            this.layoutControlItem8.Location = new System.Drawing.Point(0, 544);
+            this.layoutControlItem8.Name = "layoutControlItem8";
+            this.layoutControlItem8.Size = new System.Drawing.Size(489, 56);
+            this.layoutControlItem8.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem8.TextVisible = false;
+            // 
+            // layoutControlGroup4
+            // 
+            this.layoutControlGroup4.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup4.GroupBordersVisible = false;
+            this.layoutControlGroup4.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.결제방식});
+            this.layoutControlGroup4.Name = "layoutControlGroup4";
+            this.layoutControlGroup4.Size = new System.Drawing.Size(485, 52);
+            this.layoutControlGroup4.TextVisible = false;
+            // 
+            // cbbCode
+            // 
+            this.cbbCode.DataSource = this.bdsCode;
+            this.cbbCode.DisplayMember = "Name";
+            this.cbbCode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbCode.FormattingEnabled = true;
+            this.cbbCode.Location = new System.Drawing.Point(76, 12);
+            this.cbbCode.Name = "cbbCode";
+            this.cbbCode.Size = new System.Drawing.Size(397, 26);
+            this.cbbCode.TabIndex = 4;
+            this.cbbCode.ValueMember = "CodeID";
+            // 
+            // 결제방식
+            // 
+            this.결제방식.Control = this.cbbCode;
+            this.결제방식.Location = new System.Drawing.Point(0, 0);
+            this.결제방식.Name = "결제방식";
+            this.결제방식.Size = new System.Drawing.Size(465, 32);
+            this.결제방식.TextSize = new System.Drawing.Size(52, 18);
+            // 
+            // bdsCode
+            // 
+            this.bdsCode.DataSource = typeof(PC_Project.Data.Code);
             // 
             // BillForm
             // 
@@ -335,6 +403,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl5)).EndInit();
+            this.layoutControl5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.결제방식)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsCode)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -364,5 +438,11 @@
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem6;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem7;
+        private DevExpress.XtraLayout.LayoutControl layoutControl5;
+        private System.Windows.Forms.ComboBox cbbCode;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup4;
+        private DevExpress.XtraLayout.LayoutControlItem 결제방식;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem8;
+        private System.Windows.Forms.BindingSource bdsCode;
     }
 }
